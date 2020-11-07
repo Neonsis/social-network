45acb8e32735aa918ddf043179e4bfe0f06a53a5
Andrey Vinel <vinel.work03@gmail.com>
2020-11-07 19:39:17 +0300

    Add validation to payalods

--- social-network-rest/src/main/java/org/neonsis/socialnetwork/rest/payload/request/LoginRequest.java
+++ social-network-rest/src/main/java/org/neonsis/socialnetwork/rest/payload/request/LoginRequest.java
@@ -3,10 +3,15 @@ package org.neonsis.socialnetwork.rest.payload.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class LoginRequest {
 
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
--- social-network-rest/src/main/java/org/neonsis/socialnetwork/rest/payload/request/SignUpRequest.java
+++ social-network-rest/src/main/java/org/neonsis/socialnetwork/rest/payload/request/SignUpRequest.java
@@ -5,17 +5,33 @@ import lombok.Setter;
 import org.neonsis.socialnetwork.model.domain.user.Gender;
 import org.neonsis.socialnetwork.rest.validation.annotation.UniqueEmail;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 public class SignUpRequest {
 
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @Email
     @UniqueEmail
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
     private String password;
+
+    @NotNull
     private Gender gender;
+
+    @NotNull
     private LocalDate birthday;
 }
