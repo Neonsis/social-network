3dc8d8ee45e1209d3309d60aaaa4528f33d2ab47
Andrey Vinel <vinel.work03@gmail.com>
2020-11-07 12:35:14 +0300

    Client: Add navbar

---
+++ social-network-client/src/components/navigation/AppNavBar/AppNavBar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {IUser} from "../../../models/user";
+import {Container, Dropdown, Image, Menu, Segment} from "semantic-ui-react";
+import AvatarNotFound from "../../../assets/avatar_not_found.png";
+import {ReactComponent as Logo} from "../../../assets/logo.svg";
+import {NavLink} from "react-router-dom";
+import "./AppNavBar.scss";
+
+export type AppNavBarProps = {
+    user?: IUser;
+}
+
+export const AppNavBar = ({user}: AppNavBarProps) => {
+    return (
+        <div className="app-navbar-wrapper">
+            <Menu fixed="top" className="app-navbar" text>
+                <Container>
+                    <Menu.Item header as={NavLink} exact to="/">
+                        <Logo className="app-navbar__logo"/>
+                    </Menu.Item>
+                    {user && (
+                        <Menu.Item position="right" className="app-navbar__profile">
+                            <Dropdown
+                                trigger={
+                                    <React.Fragment>
+                                        <span className="app-navbar__profile-name">{user.firstName}</span>
+                                        <Image avatar src={AvatarNotFound}/>
+                                    </React.Fragment>
+                                }
+                                className="app-navbar__dropdown"
+                                pointing="top left"
+                            >
+                                <Dropdown.Menu>
+                                    <Dropdown.Item text='Выйти'/>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Menu.Item>
+                    )}
+                </Container>
+            </Menu>
+        </div>
+    );
+};
